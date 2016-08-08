0dcf332d2f82ab4e2254614dd53f9ebe09b26507
Ethan Law and Stephen Burden <stephen@>
2016-08-08 14:15:54 -0700

    reorganize javascript to run measurement conversions in order

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,45 @@
 var gallons = parseInt(prompt("Enter the measurement in gallons"));
-var cups = parseInt(prompt("Enter the measurment in cups"));
-var tablespoons = parseInt(prompt("Enter the measurment in tablespoons"));
-var pints = parseInt(prompt("Enter the measurment in pints"));
-var quarts = parseInt(prompt("Enter the measurement in quarts"));
-var ounces = parseInt(prompt("Enter the measurment in ounces"));
-
-
 var gallonsToLiters = function(value) {
   return value * 3.785411784;
 };
+alert (gallons + " gallons is equal to " + (gallonsToLiters(gallons)) + " liters.");
+
+var cups = parseInt(prompt("Enter the measurement in cups"));
+var cupsToTablespoons = function(value) {
+  return value * 16;
+};
+alert(cups + " cups is equal to " + (cupsToTablespoons(cups)) + " tablespoons.");
+
+var tablespoons = parseInt(prompt("Enter the measurement in tablespoons"));
+var tablespoonsToMl = function(value) {
+  return value * 15;
+};
+alert(tablespoons + " tablespoons is equal to " + (tablespoonsToMl(tablespoons)) + " milliliters.");
+
+
+var pints = parseInt(prompt("Enter the measurement in pints"));
+var pintsToCups = function(value) {
+  return value * 2;
+};
+alert(pints + " pints is equal to " + (pintsToCups(pints)) + " cups.");
+
+
+var quarts = parseInt(prompt("Enter the measurement in quarts"));
+var quartsToOunces = function(value) {
+  return value * 32;
+};
+alert (quarts + " quarts is equal to " + (quartsToOunces(quarts)) + " ounces.");
+
+
+var ounces = parseInt(prompt("Enter the measurement in ounces"));
+var ouncesToGrams = function(value) {
+  return value * 28.3495;
+};
+alert(ounces + " ounces is equal to " + (ouncesToGrams(ounces)) + " grams.");
+
 
 var litersToOunces = function(value) {
   return value * 3.785411784;
 };
-
-var quartsToOunces = function(value) {
-  return value * 32;
-};
-
-var ouncesToGrams = function(value) {
-  return value * 28.3495;
-};
-
-var cupsToTablespoons = function(value) {
-  return value * 16;
-};
-
-var tablespoonsToMl = function(value) {
-  return value * 15;
-};
-
-var pintsToCups = function(value) {
-  return value * 2;
-};
-
-
-alert(gallonsToLiters(gallons));
-alert(litersToOunces(liters));
-alert(quartsToOunces(quarts));
-alert(ouncesToGrams(ounces));
-alert(cupsToTablespoons(cups));
-alert(tablespoonsToMl(tablespoons));
-alert(pintsToCups(pints));
+var liters = parsInt(prompt("Enter the measurement in liters"));
+alert(liters + " liters is equal to " + (litersToOunces(liters)) + " ounces.");
