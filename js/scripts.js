138861cf495a200fe62ea26d8917e7e1adeea02e
Ethan Law and Stephen Burden <ethan@>
2016-08-08 13:21:19 -0700

    Add more conversions to the javascript file

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,44 @@
 var gallons = parseInt(prompt("Enter the measurement in gallons"));
+var cups = parseInt(prompt("Enter the measurment in cups"));
+var tablespoons = parseInt(prompt("Enter the measurment in tablespoons"));
+var pints = parseInt(prompt("Enter the measurment in pints"));
+var quarts = parseInt(prompt("Enter the measurement in quarts"));
+var ounces = parseInt(prompt("Enter the measurment in ounces"));
 
-var liters = function(gals) {
-  return gals * 3.785411784;
+
+var gallonsToLiters = function(value) {
+  return value * 3.785411784;
 };
 
-alert(liters(gallons));
-
-
-var ounces = parseInt(prompt("Enter the measurement in ounces"));
-
-var grams = function(oz) {
-  return oz * 28.3495;
+var litersToOunces = function(value) {
+  return value * 3.785411784;
 };
 
-alert(grams(oz));
+var quartsToOunces = function(value) {
+  return value * 32;
+};
+
+var ouncesToGrams = function(value) {
+  return value * 28.3495;
+};
+
+var cupsToTablespoons = function(value) {
+  return value * 16;
+};
+
+var tablespoonsToMl = function(value) {
+  return value * 15;
+};
+
+var pintsToCups = function(value) {
+  return value * 2;
+};
+
+
+alert(gallonsToLiters(gallons));
+alert(litersToOunces(liters));
+alert(quartsToOunces(quarts));
+alert(ouncesToGrams(ounces));
+alert(cupsToTablespoons(cups));
+alert(tablespoonsToMl(tablespoons));
+alert(pintsToCups(pints));
